2f86b947ffa0a78a322848f7d70999c5bbec5f2f
yoshi-automation <yoshi-automation@google.com>
2021-02-06 04:24:01 -0800

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- system-test/fixtures/sample/src/index.ts
+++ system-test/fixtures/sample/src/index.ts
@@ -16,7 +16,7 @@
 // ** https://github.com/googleapis/gapic-generator-typescript **
 // ** All changes to this file may be overwritten. **
 
-import {GkeHubMembershipServiceClient} from '@google-cloud/gke-hub';
+import {GkeHubMembershipServiceClient} from '@google-cloud/gkehub';
 
 // check that the client class type name can be used
 function doStuffWithGkeHubMembershipServiceClient(
